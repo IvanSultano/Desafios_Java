2a5681069f375b4078e4eb1bcddfe3cfbc28982e
Ivan Sultano <76110198+IvanSultano@users.noreply.github.com>
2023-03-25 11:06:45 -0300

    Update criador_de_email.java

--- criador_de_email.java
+++ criador_de_email.java
@@ -1,3 +1,10 @@
+/**
+    Crie uma classe para representar pessoas. O construtor da classe deve receber como parâmetros o nome completo, 
+altura e a data de nascimento. Os objetos, logo após criados, devem criar automaticamente um email no formato nome.sobrenome@dominio.
+    Desafio: A data de nascimento não pode ser posterior à data atual.
+*/
+
+
 package criador_de_email;
 
 import java.util.Date;
