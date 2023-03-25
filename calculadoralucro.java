66de5c7abe6f08263f6613b8e5bd87fd8b6cf93c
Ivan Sultano <76110198+IvanSultano@users.noreply.github.com>
2023-03-25 11:04:24 -0300

    Update calculadoralucro.java

--- calculadoralucro.java
+++ calculadoralucro.java
@@ -1,3 +1,10 @@
+/**
+    Crie uma classe que representa um produto. A classe deve ter os atributos, nome, marca, preço de custo e preço de venda.
+Deve ter também um método que calcula e retorna o lucro.
+    Crie objetos dessa classe e escreva na tela seus atributos e o valor do lucro.
+*/
+
+
 package calculadoralucro;
 
 /**
